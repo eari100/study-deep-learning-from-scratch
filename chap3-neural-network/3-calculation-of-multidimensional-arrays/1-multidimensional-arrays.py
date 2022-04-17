63f2797f09a7fd8c2f88778d53c5c0b02aa2b6fb
Jaewook Choi <eari100@naver.com>
2022-04-17 23:21:34 +0900

    파이써닉하게 수정

--- chap3-neural-network/3-calculation-of-multidimensional-arrays/1-multidimensional-arrays.py
+++ chap3-neural-network/3-calculation-of-multidimensional-arrays/1-multidimensional-arrays.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 A = np.array([1, 2, 3, 4])
 print(A)
 # 차원 수
@@ -9,7 +8,7 @@ print(np.ndim(A))
 print(A.shape)
 print(A.shape[0])
 
-B = np.array([[1,2], [3,4], [5,6]])
+B = np.array([[1, 2], [3, 4], [5, 6]])
 print(B)
 print(np.ndim(B))
 print(B.shape)

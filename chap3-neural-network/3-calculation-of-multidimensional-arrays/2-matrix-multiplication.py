aa1a338fe7f07e4a7a466e5e1ed2bf90dea3f8a0
Jaewook Choi <eari100@naver.com>
2022-04-17 23:55:59 +0900

    행렬의 곱
    - 2차원 행렬 x 1차원 행렬

--- chap3-neural-network/3-calculation-of-multidimensional-arrays/2-matrix-multiplication.py
+++ chap3-neural-network/3-calculation-of-multidimensional-arrays/2-matrix-multiplication.py
@@ -17,3 +17,9 @@ print(A.shape)
 print(C.shape)
 # ValueError
 # print(np.dot(A, C))
+
+A = np.array([[1, 2], [3, 4], [5, 6]])
+print(A.shape)
+B = np.array([7, 8])
+print(B.shape)
+print(np.dot(A, B))
